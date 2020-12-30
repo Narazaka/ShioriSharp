f9fb4659479086a7190e7d99324b2eec21c2de10
Narazaka <info@narazaka.net>
2020-12-30 09:21:21 +0900

    fix split

--- ShioriSharp/HeaderValueStringExtensions.cs
+++ ShioriSharp/HeaderValueStringExtensions.cs
@@ -7,14 +7,14 @@ namespace ShioriSharp {
 #if NETSTANDARD2_1 || NET5_0
             str.Split(separator);
 #else
-            str.Split(separator.ToCharArray());
+            str.Split(new string[] { separator }, System.StringSplitOptions.None);
 #endif
         public static string CombineValues(this IEnumerable<string?> strs, string separator = "\x01") => string.Join(separator, strs);
         public static IEnumerable<string[]> SeparateValues2(this string str, string separator1 = "\x02", string separator2 = "\x01") =>
 #if NETSTANDARD2_1 || NET5_0
             str.Split(separator1).Select(part => part.Split(separator2));
 #else
-            str.Split(separator1.ToCharArray()).Select(part => part.Split(separator2.ToCharArray()));
+            str.Split(new string[] { separator1 }, System.StringSplitOptions.None).Select(part => part.Split(new string[] { separator2 }, System.StringSplitOptions.None));
 #endif
         public static string CombineValues2(this IEnumerable<IEnumerable<string?>> strs, string separator1 = "\x02", string separator2 = "\x01") => string.Join(separator1, strs.Select(part => string.Join(separator2, part)));
     }
